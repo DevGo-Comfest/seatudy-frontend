3e747dd3d82486095abaf4eb90768e197e737dc7
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-08-07 20:33:05 +0700

    create data dummy for list data syllabus

---
+++ app/src/main/java/com/comfest/instructor/data/dummy/SyllabusDataInstructor.kt
@@ -0,0 +1,8 @@
+package com.comfest.instructor.data.dummy
+
+data class SyllabusDataInstructor(
+    val title: String,
+    val description: String,
+    val titleAssignment: String,
+    val dueDateAssignment: Int
+)
--- app/src/main/java/com/comfest/instructor/ui/sylabus/SyllabusInstructorActivity.kt
+++ app/src/main/java/com/comfest/instructor/ui/sylabus/SyllabusInstructorActivity.kt
@@ -3,14 +3,15 @@ package com.comfest.instructor.ui.sylabus
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.comfest.seatudy.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.comfest.instructor.data.dummy.SyllabusDataInstructor
+import com.comfest.instructor.ui.sylabus.adapter.SyllabusInstructorAdapter
 import com.comfest.seatudy.databinding.ActivitySyllabusInstructorBinding
 
 class SyllabusInstructorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySyllabusInstructorBinding
+    private lateinit var syllabusAdapter: SyllabusInstructorAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +21,29 @@ class SyllabusInstructorActivity : AppCompatActivity() {
         binding.ivBack.setOnClickListener {
             onBackPressed()
         }
+
+        setupRecyclerView()
+        loadedSyllabus()
+    }
+
+
+    private fun setupRecyclerView() {
+        syllabusAdapter = SyllabusInstructorAdapter()
+        binding.rvList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = syllabusAdapter
+        }
+    }
+
+    private fun loadedSyllabus() {
+        val titleDummy = "Lorem ipsum dolor sit amet"
+        val descDummy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        val sampleSyllabus = listOf(
+            SyllabusDataInstructor(titleDummy, descDummy, titleDummy, 4),
+            SyllabusDataInstructor(titleDummy, descDummy, titleDummy, 3),
+            SyllabusDataInstructor(titleDummy, descDummy, titleDummy, 1),
+            SyllabusDataInstructor(titleDummy, descDummy, titleDummy, 5),
+        )
+        syllabusAdapter.setSyllabus(sampleSyllabus)
     }
 }
