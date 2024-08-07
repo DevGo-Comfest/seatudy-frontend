f9090f3c879028e8ace03c3792ebdf463d6c695a
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-08-07 09:16:20 +0700

    create dummy data and display it on the fragment home page instructor

--- app/src/main/java/com/comfest/instructor/ui/home/HomeInstructorFragment.kt
+++ app/src/main/java/com/comfest/instructor/ui/home/HomeInstructorFragment.kt
@@ -5,56 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.comfest.instructor.data.dummy.CourseInstructor
+import com.comfest.instructor.ui.home.adapter.HomeCourseInstructorAdapter
 import com.comfest.seatudy.R
+import com.comfest.seatudy.databinding.FragmentHomeInstructorBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeInstructorFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeInstructorFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHomeInstructorBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var courseAdapter: HomeCourseInstructorAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_instructor, container, false)
+        _binding = FragmentHomeInstructorBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeInstructorFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeInstructorFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+        loadCourses()
     }
+
+
+    private fun setUpRecyclerView() {
+        courseAdapter = HomeCourseInstructorAdapter()
+        binding.rvList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = courseAdapter
+        }
+    }
+
+
+    private fun loadCourses() {
+        val sampleCourses = listOf(
+            CourseInstructor(1,  R.drawable.test_dummy ,"Software Engineer Mobile Development Android Engineer", 4, "12H"),
+            CourseInstructor(2,  R.drawable.test_dummy,"iOS Development", 4, "10H"),
+            CourseInstructor(3,  R.drawable.test_dummy,"Web Development", 5, "15H")
+        )
+        courseAdapter.setCourses(sampleCourses)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
