1352cc9d671874fd3cd7bfe4d2086ee7cc5775cf
DewaTriWijaya <dewatwc@gmail.com>
2024-08-08 13:38:45 +0700

    Update Course User

--- app/src/main/java/com/comfest/seatudy/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/comfest/seatudy/ui/dashboard/DashboardFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.comfest.seatudy.databinding.FragmentDashboardBinding
@@ -19,17 +18,9 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
+        val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
