63d838143fcfb1ceefb28cd32227f68823fa3bbb
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-08-08 10:23:43 +0700

    slicing ui layout assignment student

---
+++ app/src/main/java/com/comfest/instructor/ui/assignment/AssignmentInstructorActivity.kt
@@ -0,0 +1,16 @@
+package com.comfest.instructor.ui.assignment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.comfest.seatudy.R
+
+class AssignmentInstructorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_assignment_instructor)
+    }
+}
