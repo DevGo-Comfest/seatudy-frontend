ef3472a99c90d152131f15cd93e560992ed13e28
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-08-08 11:16:21 +0700

    create assignment adapter and create data dummy for list assignment

--- app/src/main/java/com/comfest/instructor/ui/assignment/AssignmentInstructorActivity.kt
+++ app/src/main/java/com/comfest/instructor/ui/assignment/AssignmentInstructorActivity.kt
@@ -3,14 +3,54 @@ package com.comfest.instructor.ui.assignment
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.comfest.seatudy.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.comfest.instructor.data.dummy.AssignmentInstructor
+import com.comfest.instructor.ui.assignment.adapter.AssignmentInstructorAdapter
+import com.comfest.seatudy.databinding.ActivityAssignmentInstructorBinding
 
 class AssignmentInstructorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAssignmentInstructorBinding
+    private lateinit var assignmentInstructorAdapter: AssignmentInstructorAdapter
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_assignment_instructor)
+        binding = ActivityAssignmentInstructorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivBack.setOnClickListener {
+            onBackPressed()
+        }
+
+
+        setupRecyclerView()
+        loadedAssignment()
+    }
+
+
+    private fun setupRecyclerView() {
+        assignmentInstructorAdapter = AssignmentInstructorAdapter()
+        binding.rvListAssignment.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = assignmentInstructorAdapter
+        }
+    }
+
+
+
+    private fun loadedAssignment() {
+        val nameStudent = "Joko Anwar"
+        val linkAttachment = "https:test123"
+        val dateSubmit = "10-11-2024"
+        val sampleAssignment = listOf(
+            AssignmentInstructor(nameStudent, linkAttachment, dateSubmit),
+            AssignmentInstructor(nameStudent, linkAttachment, dateSubmit),
+            AssignmentInstructor(nameStudent, linkAttachment, dateSubmit),
+            AssignmentInstructor(nameStudent, linkAttachment, dateSubmit),
+            AssignmentInstructor(nameStudent, linkAttachment, dateSubmit),
+        )
+
+        assignmentInstructorAdapter.setAssignment(sampleAssignment)
     }
 }
---
+++ app/src/main/java/com/comfest/instructor/ui/assignment/adapter/AssignmentInstructorAdapter.kt
@@ -0,0 +1,43 @@
+package com.comfest.instructor.ui.assignment.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.comfest.instructor.data.dummy.AssignmentInstructor
+import com.comfest.seatudy.databinding.ItemAssignmentInstructorBinding
+
+class AssignmentInstructorAdapter: RecyclerView.Adapter<AssignmentInstructorAdapter.AssignmentInstructorViewHolder>() {
+
+    private var assignment: List<AssignmentInstructor> = listOf()
+
+    fun setAssignment(newAssignment: List<AssignmentInstructor>) {
+        assignment = newAssignment
+        notifyDataSetChanged()
+    }
+
+    inner class AssignmentInstructorViewHolder(private val binding: ItemAssignmentInstructorBinding): RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(assignmentData: AssignmentInstructor) {
+            binding.apply {
+                tvNameStudent.text = assignmentData.nameStudent
+                tvAttachmentStudent.text = assignmentData.linkAttachment
+                submitedAt.text = assignmentData.dateSubmit
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AssignmentInstructorViewHolder {
+        val binding = ItemAssignmentInstructorBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return  AssignmentInstructorViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = assignment.size
+
+    override fun onBindViewHolder(holder: AssignmentInstructorViewHolder, position: Int) {
+        holder.bind(assignment[position])
+    }
+}
