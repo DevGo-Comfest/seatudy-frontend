003adfd7806c8dd438957c3eed4bba6b3491c320
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-08-09 20:49:25 +0700

    create detail syllabus and slicing ui form add syllabus material

---
+++ app/src/main/java/com/comfest/instructor/ui/sylabus/SyllabusMaterialInstructorActivity.kt
@@ -0,0 +1,21 @@
+package com.comfest.instructor.ui.sylabus
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.comfest.seatudy.R
+
+class SyllabusMaterialInstructorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_syllabus_material_instructor)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
