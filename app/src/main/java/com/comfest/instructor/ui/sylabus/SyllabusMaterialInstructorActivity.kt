ef1eb96977c6bf1e074d84191a975c836ac0f21a
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-08-10 16:31:42 +0700

    slicing ui syllabus material and add data dummy for list syllabus material

--- app/src/main/java/com/comfest/instructor/ui/sylabus/SyllabusMaterialInstructorActivity.kt
+++ app/src/main/java/com/comfest/instructor/ui/sylabus/SyllabusMaterialInstructorActivity.kt
@@ -5,17 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.comfest.instructor.data.dummy.SyllabusMaterialInstructor
+import com.comfest.instructor.ui.sylabus.adapter.SyllabusMaterialInstructorAdapter
 import com.comfest.seatudy.R
+import com.comfest.seatudy.databinding.ActivitySyllabusMaterialInstructorBinding
 
 class SyllabusMaterialInstructorActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySyllabusMaterialInstructorBinding
+    private lateinit var syllabusAdapter: SyllabusMaterialInstructorAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_syllabus_material_instructor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySyllabusMaterialInstructorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setupRecyclerView()
+        loadSyllabusMaterial()
+
+    }
+
+
+    fun setupRecyclerView() {
+        syllabusAdapter = SyllabusMaterialInstructorAdapter()
+
+        binding.rvList.apply {
+            layoutManager = LinearLayoutManager(this@SyllabusMaterialInstructorActivity)
+            adapter = syllabusAdapter
         }
+    }
+
+    fun loadSyllabusMaterial() {
+        val dummyMaterial = listOf(
+            SyllabusMaterialInstructor(
+                title = "Software Enginering",
+                desc = "Learn the principles of software engineering.",
+                linkMaterial = "https://youtu.be/0BIaPnxfYf8?si=zd-PYWLuJbGLxFeu",
+            ),
+
+            SyllabusMaterialInstructor(
+                title = "Design System",
+                desc = "Learn the principles of software engineering.",
+                linkMaterial = "https://youtu.be/XhF9LnpNtqg?si=jEGgylW3U4vre37s",
+            ),
+        )
+
+        syllabusAdapter.setSyllabusMaterial(dummyMaterial)
     }
 }
