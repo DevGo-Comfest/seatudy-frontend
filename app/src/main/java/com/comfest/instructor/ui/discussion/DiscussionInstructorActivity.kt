2efd9331c7d500371bee62df99b5ef9ad74d2e1a
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-08-10 17:15:56 +0700

    slicing ui discussion instructor

---
+++ app/src/main/java/com/comfest/instructor/ui/discussion/DiscussionInstructorActivity.kt
@@ -0,0 +1,21 @@
+package com.comfest.instructor.ui.discussion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.comfest.seatudy.R
+
+class DiscussionInstructorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_discussion_instructor)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
